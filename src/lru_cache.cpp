26ac822729336eaf1a052d3ce3b8cefae56aea9e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-11 09:31:52 +0530

    Added LRUCache and Node structure using doubly linked list

--- src/lru_cache.cpp
+++ src/lru_cache.cpp
@@ -1,2 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct LRUCache {
+    struct Node {
+        int key, val;
+        Node *prev, *next;
+        Node(int k, int v): key(k), val(v), prev(nullptr), next(nullptr) {}
+    };
