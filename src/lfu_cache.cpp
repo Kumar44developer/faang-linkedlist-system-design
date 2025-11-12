c21c165895e47e86deff968ec4d84cd3ccc422ef
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-12 10:56:47 +0530

    Update lfu_cache.cpp by defining the Node data structure using Struct

--- src/lfu_cache.cpp
+++ src/lfu_cache.cpp
@@ -1,2 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct LFUCache {
+    struct Node {
+        int key, val, freq;
+        Node *prev, *next;
+        Node(int k,int v): key(k), val(v), freq(1), prev(nullptr), next(nullptr){}
+    };
